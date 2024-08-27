8a9a2bfaee661144c277b86ae8aed13c775b9614
Trivial-Estadistico-Uco <trivialestadisticouco@gmail.com>
2024-08-27 13:27:42 +0200

    Prueba de lo que tengo hecho
    Prueba 1

---
+++ data/data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        question: 'Cual de las siguientes no es un editor de codigo',
+        choices: [
+            'vim',
+            'vscode',
+            'emacs',
+            'word',
+        ],
+        answer: 'word'
+    },
+    {
+        question: ' si editor de codigo',
+        choices: [
+            'vim',
+            'e',
+            'f',
+            'g'
+        ],
+        answer: 'vim'
+    },
+    {
+        question: 'no es un editor de codigo',
+        choices: [
+            'a',
+            'd',
+            'f',
+            'word'
+        ],
+        answer: 'word'
+    },
+];
+
